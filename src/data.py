e3655231c5a182165512ce925a6dc5a232e5219e
marlo-80 <erster.neuer@gmail.com>
2026-05-12 18:10:57 +0200

    postgres, dbt

---
+++ src/data.py
@@ -0,0 +1,51 @@
+import os
+import re
+import kagglehub
+import pandas as pd
+
+
+kagglehub_path = "robikscube/flight-delay-dataset-20182022"
+
+DEFAULT_PATH = "flight_data"
+
+def load_from_kaggle( kaggle_path: str, output_dir: str)->str:
+    #print(f"Downloading dataset from Kaggle: {kaggle_path} to {output_dir}")
+    path = kagglehub.dataset_download(kaggle_path, output_dir = output_dir)
+    print("Path to dataset files:", path)
+    return path
+
+def load_from_local(path = f"./{DEFAULT_PATH}/")->pd.DataFrame:
+    if path is None or path == "":
+        path = f"./{DEFAULT_PATH}/"
+
+    if not os.path.exists(path) or os.listdir(path) == []:
+        path = load_from_kaggle(kagglehub_path, output_dir=path)
+
+    df = pd.DataFrame()
+    for file in os.listdir(path):
+        print(f"checking file: {file}")
+        if re.match(r"Combined_Flights_\d{4}\.csv", file) is None:
+            continue
+        print( f"read from file: {file}")
+        df_ = pd.read_csv(os.path.join(path, file), 
+                    usecols=["FlightDate", "Airline", "Origin", "Dest", 
+                              "CRSDepTime", "CRSArrTime", "DepDelay", "Diverted", "Cancelled",
+                              "Operating_Airline", "OriginCityName", "DestCityName",
+                              "Tail_Number", "Flight_Number_Operating_Airline", 
+                              "OriginAirportID", "DestAirportID", "DepDelayMinutes", "ArrDelay"])        
+        
+        df = pd.concat([df, df_], ignore_index=True)        
+        
+    #print(df.head())
+    #print(df.describe())
+    #df.info()
+    return df
+
+def shackle_dataset(big_df: pd.DataFrame, fractions : list[float], max_rows: int)-> pd.DataFrame:
+    for fraction in fractions:
+        # simply split randomly the dataset and keep only the fraction of it, 
+        # then repeat until we have enough rows or we have exhausted the fractions list
+        big_df, _  = train_test_split(big_df, test_size=fraction, random_state=0xdeadbeef)
+        if not max_rows is None and len(big_df) < max_rows:
+            break
+    return big_df.reset_index(drop=True)
