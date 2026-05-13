8858fca232c1a059175a4db2ce44717a912f7abe
marlo-80 <erster.neuer@gmail.com>
2026-05-13 13:53:48 +0200

    prometheus + grafana

--- src/api.py
+++ src/api.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+@app.post("/predict")
+async def predict(data: dict):
+    return {"delay": 0.0}
+
+# Instrumentator am Ende einbinden (manchmal nötig)
+Instrumentator().instrument(app).expose(app)
